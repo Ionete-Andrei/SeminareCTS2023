afe43c9a1bd70fe33a7befec4c7a6c850865bde7
Ionete Andrei <andurey@gmail.com>
2023-02-28 14:56:51 +0200

    initial commit

--- src/src/cts/s02/principii_clean_code/main/Program.java
+++ src/src/cts/s02/principii_clean_code/main/Program.java
@@ -3,16 +3,17 @@ package src.cts.s02.principii_clean_code.main;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import src.cts.s02.principii_clean_code.clase.Angajat;
-import src.cts.s02.principii_clean_code.clase.Utils;
+import src.cts.s02.principii_clean_code.clase.Aplicant;
+import src.cts.s02.principii_clean_code.clase.readers.ReaderAngajat;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
+		ReaderAngajat readerAngajat = new ReaderAngajat();
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = readerAngajat.readAplicants("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
