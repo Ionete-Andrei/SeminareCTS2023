880e11cb2fe9ae054f4498b13ff256662bfd6853
Ionete Andrei <andurey@gmail.com>
2023-03-07 15:00:19 +0200

    Commit Sem 3

--- src/src/cts/s02/principii_clean_code/clase/Aplicant.java
+++ src/src/cts/s02/principii_clean_code/clase/Aplicant.java
@@ -1,5 +1,7 @@
 package src.cts.s02.principii_clean_code.clase;
 
+import java.util.Arrays;
+
 public abstract class Aplicant{
 	protected String nume;
 	protected String prenume;
@@ -7,6 +9,8 @@ public abstract class Aplicant{
 	protected int punctaj;
 	protected int nr_proiecte;
 	protected String[] denumireProiect;
+	static int pragAcceptare = 80;
+
 	
 	
 	public String getNume() {
@@ -27,12 +31,13 @@ public abstract class Aplicant{
 	public void setVarsta(int varsta) {
 		this.varsta = varsta;
 	}
-	public void statut(){
-		if(punctaj>80)
-			System.out.println("Aplicantul "+nume+" "+prenume+" a fost acceptat.");
-		else
-			System.out.println("Aplicantul "+nume+" "+prenume+" nu a fost acceptat.");
-		}
+
+	public void afisareStatus(){
+		System.out.println("Aplicantul "+this.nume+" "+this.prenume+
+							(this.punctaj >= Aplicant.pragAcceptare?"":" nu")
+							+" a fost acceptat/a.");
+	}
+
 	public int getPunctaj() {
 		return punctaj;
 	}
@@ -60,8 +65,41 @@ public abstract class Aplicant{
 	public int getNr_proiecte() {
 		return nr_proiecte;
 	}
+
 	public void setNr_proiecte(int nr_proiecte) {
 		this.nr_proiecte = nr_proiecte;
+	}
+
+	public void setDenumireProiecte(String denumiri[]){
+		this.denumireProiect = new String[nr_proiecte];
+		for(int i = 0; i<nr_proiecte; i++){
+			this.denumireProiect[i] = denumiri[i];
+		}
+	}
+
+	public abstract void afisareFinantare();
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("");
+		sb.append("nume='").append(nume).append('\'');
+		sb.append(", prenume='").append(prenume).append('\'');
+		sb.append(", varsta=").append(varsta);
+		sb.append(", punctaj=").append(punctaj);
+		sb.append(", nr_proiecte=").append(nr_proiecte);
+		sb.append(", denumireProiect=").append(Arrays.toString(denumireProiect));
+		return sb.toString();
+	}
+
+	protected String afisareFinantareNume(int sumaFinantata){
+		final StringBuilder sbf = new StringBuilder("");
+		sbf.append(nume);
+		sbf.append(" ");
+		sbf.append(prenume);
+		sbf.append(" primeste ");
+		sbf.append(sumaFinantata);
+		sbf.append(" Euro/zi in proiect.");
+		return sbf.toString();
 	}
 
 }
